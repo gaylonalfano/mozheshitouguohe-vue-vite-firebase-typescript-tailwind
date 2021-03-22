5f40c11f57c1b92333df365fc3dff42153e55b5a
gaylonalfano <gaylon.alfano@gmail.com>
2021-03-22 20:35:29 +0800

    Adding some simple transitions throughout the Navbar after learning form the Vue Transitions series.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -78,7 +78,7 @@
           <button
             @click="toggleDropdown"
             type="button"
-            class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 transition ease-out duration-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
             aria-controls="mobile-menu"
             aria-expanded="false"
           >
@@ -139,7 +139,7 @@
         <div class="hidden lg:block lg:ml-4">
           <div class="flex items-center">
             <button
-              class="flex-shrink-0 p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              class="flex-shrink-0 p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white transition ease-out duration-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
             >
               <span class="sr-only">View notifications</span>
               <!-- Heroicon name: outline/bell -->
@@ -200,32 +200,41 @@
                     NOTE: Make this z-index LOWER from backdrop/mask!
                     Otherwise you can't click the modal elements!
                   -->
-              <div
-                v-show="dropdownOpen"
-                class="absolute right-0 z-20 w-48 py-1 mt-2 bg-white shadow-lg origin-top-right rounded-md ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu"
+              <transition
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                enter-active-class="transition ease-out duration-100"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+                leave-active-class="transition ease-in duration-75"
               >
-                <router-link
-                  :to="{ name: 'Profile' }"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
-                  role="menuitem"
-                  >Your Profile</router-link
+                <div
+                  v-show="dropdownOpen"
+                  class="absolute right-0 z-20 w-48 py-1 mt-2 bg-white shadow-lg origin-top-right rounded-md ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu"
                 >
-                <router-link
-                  :to="{ name: 'Settings' }"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
-                  role="menuitem"
-                  >Settings</router-link
-                >
-                <span
-                  @click="handleLogout"
-                  class="block px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-200"
-                >
-                  Sign out
-                </span>
-              </div>
+                  <router-link
+                    :to="{ name: 'Profile' }"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
+                    role="menuitem"
+                    >Your Profile</router-link
+                  >
+                  <router-link
+                    :to="{ name: 'Settings' }"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
+                    role="menuitem"
+                    >Settings</router-link
+                  >
+                  <span
+                    @click="handleLogout"
+                    class="block px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-200"
+                  >
+                    Sign out
+                  </span>
+                </div>
+              </transition>
             </div>
           </div>
         </div>
@@ -274,7 +283,7 @@
             </div>
           </div>
           <button
-            class="flex-shrink-0 p-1 ml-auto text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+            class="flex-shrink-0 p-1 ml-auto text-gray-400 bg-gray-800 rounded-full hover:text-white transition ease-out duration-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
           >
             <span class="sr-only">View notifications</span>
             <!-- Heroicon name: outline/bell -->
@@ -312,12 +321,12 @@
           <!--   class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700" -->
           <!--   >Sign out</router-link -->
           <!-- > -->
-          <button
+          <span
             @click="handleLogout"
-            class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700"
+            class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md cursor-pointer hover:text-white hover:bg-gray-700 transition ease-out duration-500"
           >
             Sign out
-          </button>
+          </span>
         </div>
       </div>
     </div>
@@ -350,7 +359,7 @@ export default defineComponent({
     // Create some active/inactive CSS classes
     const activeClass = ref<string>("bg-gray-900 text-white");
     const inactiveClass = ref<string>(
-      "text-gray-300 hover:bg-gray-700 hover:text-white"
+      "text-gray-300 hover:bg-gray-700 hover:text-white transition ease-out duration-500"
     );
 
     const router = useRouter();
